a4eda4f634a52e1cba132cb7fae80d18e3924211
Caitlin Stein <caitlin.teter@gmail.com>
2016-03-08 09:56:38 -0600

    initial commit

---
+++ firebase.md
@@ -0,0 +1,18 @@
+
+0. Make sure you have a google account
+1. go to firebase.com
+2. click on sign  up with google (upper right hand corner)
+3. import properly formatted .json file
+  i. an un-named object with one (or more) array of objects, but really, just copy and paste something that already works
+4. create ajax call using  your unique firsebase address
+  i. Don't forget to use sweet sweet promises
+  ii. Yeah, um, I guess it would work with XHR, but go ahead and learn ajax.
+5. Enjoy popoulating the DOM as you're accustomed****
+
+To Add to Firebase...
+
+6. Create an object that you want to add, it should probably match the format of the objects that are currently in your firebase.
+  i. use the jQuery, Luke.
+  ii. Don't bother with form tags, just trust me
+7. Write a new ajax call with a "post" method, don't forget to include "data." You will post to the same url as before.
+8. 
---
+++ script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+const $ = require('jquery');
+
+function display(data) {
+  let displayedAnimals = "";
+  for (var prop in data) {
+    displayedAnimals += `<div class="animalInfo">`;
+    displayedAnimals += `<h2>${data[prop].animal}</h2>`;
+    displayedAnimals += `<img src="${data[prop].image}">`;
+    displayedAnimals += `<p>Distinguishing characteristic: ${data[prop].characteristic}</p>`;
+    console.log("data[i].animal", data[prop].animal);
+    displayedAnimals += `</div>`;
+  }
+  $("#animals").html(displayedAnimals);
+}
+
+let getAnimals = function() {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+            url:"https://vivid-heat-6487.firebaseio.com/animals.json",//.json tells firebase to send back in json
+            method: "GET",
+          }).done(function(data) {
+              resolve(data);
+              //console.log("data", data); 
+          }).fail(function(error) {
+              reject(error);
+              console.log("failed", error);
+          });
+  });
+};
+
+getAnimals()
+  .then(
+    function(data) {
+      console.log("data", data);
+      display(data);
+    }
+  );
+/// build new animal
+
+$("#button").click(function() {
+  console.log("you clicked the button!");
+  let newAnimal = {
+    "animal": $("#animal").val(),
+    "image": $("#image").val(),
+    "characteristic": $("#characteristic").val()
+  };
+  console.log("newAnimal", newAnimal);
+  newAnimal = JSON.stringify(newAnimal);
+  $.ajax({
+        url:"https://vivid-heat-6487.firebaseio.com/animals.json",
+        method: "POST",
+        data: newAnimal
+      }).done(function(addedAnimal){
+          console.log("addedAnimal", addedAnimal);
+     
+      });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
